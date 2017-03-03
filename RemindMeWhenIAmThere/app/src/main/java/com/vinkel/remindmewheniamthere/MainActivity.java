4cbf7c64cac9cfa42214468e28b670eddea1e306
Def4l71diot <lubeto3099@gmail.com>
2017-03-03 14:29:08 +0200

    Post-merge fix

--- RemindMeWhenIAmThere/app/src/main/java/com/vinkel/remindmewheniamthere/MainActivity.java
+++ RemindMeWhenIAmThere/app/src/main/java/com/vinkel/remindmewheniamthere/MainActivity.java
@@ -9,12 +9,7 @@ import com.vinkel.remindmewheniamthere.providers.base.IIntentFactory;
 import com.vinkel.remindmewheniamthere.utils.base.IApplicationSettingsManager;
 import com.vinkel.remindmewheniamthere.views.home.HomeActivity;
 import com.vinkel.remindmewheniamthere.views.intro.IntroActivity;
-<<<<<<< HEAD
-=======
-import com.vinkel.remindmewheniamthere.views.sign_in.SignInActivity;
-import com.vinkel.remindmewheniamthere.views.sign_up.SignUpActivity;
 
->>>>>>> 6723dc6883d88268d668b13134298c5487161144
 import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,11 +29,7 @@ public class MainActivity extends AppCompatActivity {
     if (applicationSettingsManager.getIsFirstLaunch()) {
       intent = intentFactory.getIntent(IntroActivity.class);
     } else {
-<<<<<<< HEAD
       intent = intentFactory.getIntent(HomeActivity.class);
-=======
-      intent = intentFactory.getIntent(SignInActivity.class);
->>>>>>> 6723dc6883d88268d668b13134298c5487161144
     }
     this.startActivity(intent);
     this.finish();
